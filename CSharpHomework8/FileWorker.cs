a87eead33093601a38fe82fb4edaf8b2ff34ea96
Ivan <IvanBWork@yandex.ru>
2024-08-22 03:51:36 +0300

    Добавьте файлы проекта.

---
+++ CSharpHomework8/FileWorker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework8
+{
+    internal class FileWorker
+    {
+        public static bool FindWord(string path, string word)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var tempStr = sr.ReadLine();
+
+                    if (tempStr.Contains(word))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public static void FindFiles(string path, string fileExtension, List<string> list)
+        {
+            list.AddRange(Directory.GetFiles(path, fileExtension));
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                FindFiles(dir, fileExtension, list);
+            }
+        }
+    }
+}
---
+++ CSharpHomework8/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpHomework8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            FileWorker.FindFiles("D:\\GeekBrains\\C#", "*.txt", list);
+
+            if (args.Length == 1)
+            {
+                foreach (string file in list)
+                {
+                    if (FileWorker.FindWord(file, args[0]) == true)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("Укажите искомое слово: ");
+                string text = Console.ReadLine()!;
+
+                foreach (string file in list)
+                {
+                    if (FileWorker.FindWord(file, text) == true)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+            }
+        }
+    }
+}
